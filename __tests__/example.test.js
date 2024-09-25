da17c63048094f33bfc1e600dcdefd04bfc8fd6d
NgomSysOps <ngom4675@gmail.com>
2024-09-25 16:55:55 +0000

    test1

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -4,6 +4,6 @@ function add(a, b) {
   return a + b;
 }
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test('adds 5 + 5 to equal 10', () => {
+  expect(add(5, 5)).toBe(10);
 });
